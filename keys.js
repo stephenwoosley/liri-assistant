f547848d6ab115e912cbb57d5a3126effd973582
Stephen Woosley <swoosley@sharptop.co>
2017-02-19 00:18:35 -0500

    first commit, rather sizeable

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('keys are loaded');
+
+var twitterKeys = {
+  consumer_key: 'EvderuPY014LKv56GzNGjflQ8',
+  consumer_secret: '52JkIviiMBXSQLc40OWgZ5FFnB722qKRXhSOar4JnkvDkQBwvH',
+  access_token_key: '15626507-BU7rnnjLGS1Vm0QStwjxP6KI7XpunrIUZytcpU2NK',
+  access_token_secret: 'q87fPbk47uu4Dal7HIgeGzk8pq9tmu73ni3ab81PspxJY',
+}
+
+module.exports = twitterKeys;
